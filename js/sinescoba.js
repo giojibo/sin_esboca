bbaa0ae8243f024f417e70aff844d843a0620511
giojibo <bonillagiovani6@gmail.com>
2024-03-13 13:45:06 -0600

    Archivos de barriendo sin escoba

---
+++ js/sinescoba.js
@@ -0,0 +1,11 @@
+function mostrarReproductor() 
+{
+	var reproductor = document.getElementById("reproductor");
+	reproductor.style.display = "block";
+}
+
+function mostrarVideo()
+	{
+		var video = document.getElementById("video");
+		video.style.display = "block";
+	}
